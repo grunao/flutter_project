97463418963ee1787f3afd4019428efbb83afa2c
Dima <a@a.a>
2025-04-16 18:12:40 +0300

    фывапролдж

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  // 123456789
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(home: Scaffold(body: Center(child: Text(''))));
